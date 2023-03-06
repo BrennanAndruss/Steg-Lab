f918cd494730c3182463b6a81c0da5f98c2b9987
Andruss <brennana46274@stu.powayusd.com>
2023-03-06 12:38:42 -0800

    day 1

---
+++ PixelPlayground.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+
+public class PixelPlayground
+{
+    /** Method to set the blue values of each pixel to 0 */
+    public static Picture zeroBlue(Picture pic)
+    {
+        Pixel[][] pixels = Picture.getPixels2D();
+    }
+}
